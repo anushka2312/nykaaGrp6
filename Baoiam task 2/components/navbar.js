8ec0565e80404503f2880be0407cfe308f7e31f8
anushka2312 <anushkachillal23@gmail.com>
2023-03-26 02:20:30 +0530

    changed

---
+++ Baoiam task 2/components/navbar.js
@@ -0,0 +1,14 @@
+function navbar() {
+    return ` 
+     <!-- navbar -->
+     <div id="navbar-middle">
+     <div id="logo">
+       <img id="logo-img" src="https://seeklogo.com/images/N/nykaa-logo-AA06314FB9-seeklogo.com.png">
+      </div>
+      <div class="nav-middle-list">
+            <li><a href="#">HOME</li>
+            </a>
+            
+     </div>`
+ }
+ export {navbar}
